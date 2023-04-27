017c48b8434de98d072e35b465b7a25c806bb068
Gleb <g.elenev@gmail.com>
2023-04-27 18:11:00 +0300

    refactor: move operations to the functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,26 +4,17 @@ const path = require('path');
 const targetDir = path.join(__dirname, 'files-copy');
 const srcDir = path.join(__dirname, 'files');
 
-fs.access(targetDir, (err) => {
-  if (err) fs.mkdir(targetDir, (err) => { 
-    if (err) throw err;
-  });
-  fs.readdir(targetDir, (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-      fs.unlink(path.join(targetDir, file), (err) => {
-        if (err) throw err;
+async function copyDir(src, target) {
+  await fs.promises.rm(target, { force: true, recursive: true });
+  await fs.promises.mkdir(target);
+  const files = await fs.promises.readdir(src, { withFileTypes: true })
+  for (file of files) {
+    if (file.isFile()) {
+      fs.copyFile(path.join(src, file.name), path.join(target, file.name), (err) => {
+        if (err) console.log(err);
       });
-    })
-  })
-  fs.readdir(srcDir, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    files.forEach((file) => {
-      if (file.isFile()) {
-        fs.copyFile(path.join(srcDir, file.name), path.join(targetDir, file.name), (err) => {
-          if (err) throw err;
-        });
-      }
-    });
-  })
-}); 
+    }
+  }
+}
+
+copyDir(srcDir, targetDir);
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,23 +4,28 @@ const path = require('path');
 const srcDir = path.join(__dirname, 'styles');
 const targetDir = path.join(__dirname, 'project-dist');
 
-fs.readdir(srcDir, { withFileTypes: true }, (err, files) => {
-  if (err) throw err;
-  let wholeData = [];
-  fs.writeFile(path.join(targetDir, 'bundle.css'), '', (err) => {
+
+function mergeFiles(src, target, name) {
+  fs.readdir(src, { withFileTypes: true }, (err, files) => {
     if (err) throw err;
-  })
-  files.forEach((file) => {
-    if (file.isFile() && path.extname(file.name) === '.css') {
-      const stream = fs.createReadStream(path.join(srcDir, file.name), 'utf-8'); 
-      stream.on('data', (data) => {
-        wholeData.push(data);
-      });
-      stream.on('end', () => {
-        fs.appendFile(path.join(targetDir, 'bundle.css'), wholeData.join('\n'), 'utf-8', (error) => {
-          if (error) throw error;
+    let wholeData = [];
+    fs.writeFile(path.join(target, `${name}`), '', (err) => {
+      if (err) throw err;
+    })
+    files.forEach((file) => {
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        const stream = fs.createReadStream(path.join(src, file.name), 'utf-8'); 
+        stream.on('data', (data) => {
+          wholeData.push(data);
         });
-      })
-    }
+        stream.on('end', () => {
+          fs.appendFile(path.join(target, `${name}`), wholeData.join('\n'), 'utf-8', (error) => {
+            if (error) throw error;
+          });
+        })
+      }
+    });
   });
-});
+}
+
+mergeFiles(srcDir, targetDir, 'bundle.css');
